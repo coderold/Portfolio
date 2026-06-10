f07c5197d029bd674f08eedff1c4f81174efb71c
Matthew Molina <mm35molina@gmail.com>
2026-06-10 22:38:14 +0800

    feat: add lucide react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
+import { Camera } from 'lucide-react'
 import './index.css'
 
 function App() {
   return (
     <>
-      <h1 className="text-red-500 font-bold text-3xl">
+      <h1 className="text-red-500 text-3xl">
         Hello world!
       </h1>
+      <Camera className="text-blue-500" />
     </>
   )
 }
